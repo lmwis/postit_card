c3c95f6441739c34888a9089b4de5e1b0352fe78
lmwis <detectiveygn@163.com>
2022-01-01 23:47:55 +0800

    init

---
+++ src/main/java/com/olivia/app/PostitCardApplication.java
@@ -0,0 +1,17 @@
+package com.olivia.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: TODO
+ * @Author: lmwis
+ * @Data: 2022/1/1 9:34 下午
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class PostitCardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PostitCardApplication.class,args);
+    }
+}
